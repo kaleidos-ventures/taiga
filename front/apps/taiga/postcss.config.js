ac089dbb80db09638f68ae8ae2134e6c4e94f5ff
Juanfran <juanfran.ag@gmail.com>
2021-02-23 10:19:24 +0100

    feat(core): postcss

---
+++ front/apps/taiga/postcss.config.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2014-2021 Taiga Agile LLC
+ *
+ * This source code is licensed under the terms of the
+ * GNU Affero General Public License found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+module.exports = {
+  'plugins': [
+    require('postcss-import')({
+      from: "/src/styles.css"
+    }),
+    require('postcss-mixins'),
+    require('postcss-preset-env')({
+      stage: 0
+    })
+  ]
+}
---
+++ front/extra-webpack.config.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2014-2021 Taiga Agile LLC
+ *
+ * This source code is licensed under the terms of the
+ * GNU Affero General Public License found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+import * as webpack from 'webpack';
+
+export default function(config: webpack.WebpackOptions) {
+  if (config.module) {
+    config.module.rules.push(
+      {
+        test   : /\.css$/,
+        loader : 'postcss-loader',
+      }
+    );
+  }
+
+  return config;
+}

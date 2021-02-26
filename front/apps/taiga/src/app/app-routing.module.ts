67c5eedfc88b6243a85782151dde61ed6cabea0e
Juanfran <juanfran.ag@gmail.com>
2021-02-26 11:21:29 +0100

    feat(core): routing module

---
+++ front/apps/taiga/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2014-2021 Taiga Agile LLC
+ *
+ * This source code is licensed under the terms of the
+ * GNU Affero General Public License found in the LICENSE file in
+ * the root directory of this source tree.
+ */
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
--- front/apps/taiga/src/app/app.module.ts
+++ front/apps/taiga/src/app/app.module.ts
@@ -14,17 +14,24 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    StoreRouterConnectingModule.forRoot(),
     StoreModule.forRoot(
       {},
       {
         metaReducers: !environment.production ? [] : [],
         runtimeChecks: {
-          strictActionImmutability: true,
           strictStateImmutability: true,
+          strictActionImmutability: true,
+          strictStateSerializability: true,
+          strictActionSerializability: true,
+          strictActionTypeUniqueness: true,
         },
       }
     ),

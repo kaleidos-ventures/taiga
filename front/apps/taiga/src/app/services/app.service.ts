13900811a4b1064e2d9d78b14dcfb139c246dac5
Juanfran <juanfran.ag@gmail.com>
2021-03-02 10:30:39 +0100

    fix(app): unexpectedHttpErrorResponseAction

--- front/apps/taiga/src/app/services/app.service.ts
+++ front/apps/taiga/src/app/services/app.service.ts
@@ -8,16 +8,15 @@
 
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Store } from '@ngrx/store';
 import { unexpectedError } from '@/app/app.actions';
 
 @Injectable()
 export class AppService {
-  constructor(private store: Store) {}
-
-  public unexpectedHttpErrorResponse(error: HttpErrorResponse) {
-    this.store.dispatch(unexpectedError({error: {
-      message: error.message,
-    }}));
+  public unexpectedHttpErrorResponseAction(error: HttpErrorResponse) {
+    return unexpectedError({
+      error: {
+        message: error.message,
+      }
+    });
   }
 }

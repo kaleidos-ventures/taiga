ae2ca49f0eb64fa1ca328c114826b1b96b4c55b9
Juanfran <juanfran.ag@gmail.com>
2021-03-15 08:32:14 +0100

    fix(ssr): mock wsService in ssr

--- front/apps/taiga/src/app/app.server.module.ts
+++ front/apps/taiga/src/app/app.server.module.ts
@@ -11,11 +11,21 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { WsService } from '@taiga/ws';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
+  ],
+  providers: [
+    {
+      provide: WsService, useValue: {
+        listen: () => {
+          return undefined;
+        }
+      }
+    }
   ],
   bootstrap: [AppComponent],
 })

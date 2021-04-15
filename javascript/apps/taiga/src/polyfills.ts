4c7a6180e9608d0a32dd03abd8e89f6d68dc6577
teresa.delatorre <teresa.delatorre@kaleidos.net>
2021-04-15 07:54:44 +0200

    Apply license change to MPL based

--- javascript/apps/taiga/src/polyfills.ts
+++ javascript/apps/taiga/src/polyfills.ts
@@ -1,4 +1,12 @@
 /**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2021-present Kaleidos Ventures SL
+ */
+
+/**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
  *
--- javascript/scripts/concat-dist-files.sh
+++ javascript/scripts/concat-dist-files.sh
@@ -1,2 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2021-present Kaleidos Ventures SL
+
 #!/bin/bash
 cat ./dist/elements/runtime-es5.js ./dist/elements/polyfills-es5.js ./dist/elements/main-es5.js > ./dist/elements/elements.js && ls -lah ./dist/elements/elements.js

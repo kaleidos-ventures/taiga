81dbc56432ce224df7ceaf7fdc2342f2e2ff1a33
Juanfran <juanfran.ag@gmail.com>
2021-08-19 08:58:29 +0200

    chore: upgrade

--- javascript/jest.config.js
+++ javascript/jest.config.js
@@ -1,3 +1,5 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -6,13 +8,4 @@
  * Copyright (c) 2021-present Kaleidos Ventures SL
  */
 
-module.exports = {
-  projects: [
-    '<rootDir>/apps/taiga',
-    '<rootDir>/libs/ui',
-    '<rootDir>/libs/api',
-    '<rootDir>/libs/data',
-    '<rootDir>/libs/ws',
-    '<rootDir>/libs/core',
-  ],
-};
+module.exports = { projects: getJestProjects() };

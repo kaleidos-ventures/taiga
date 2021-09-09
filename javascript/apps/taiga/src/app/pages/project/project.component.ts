077871713670d149cc603dedd51c755728c06519
Xaviju <xaviju@gmail.com>
2021-09-09 08:19:25 +0200

    feat: project page

---
+++ javascript/apps/taiga/src/app/pages/project/project.component.ts
@@ -0,0 +1,24 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2021-present Kaleidos Ventures SL
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tg-project',
+  templateUrl: './project.component.html',
+  styleUrls: ['./project.component.css']
+})
+export class ProjectComponent implements OnInit {
+
+  // constructor() {}
+
+  public ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+}

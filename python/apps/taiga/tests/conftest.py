219d4254cc44a65392b26a3cffc15d4d89f1d284
David Barragán Merino <david.barragan@kaleidos.net>
2021-11-03 12:46:10 +0100

    chore(tests): add params to run only slow or fast tests

--- python/apps/taiga/tests/conftest.py
+++ python/apps/taiga/tests/conftest.py
@@ -11,7 +11,41 @@ from django.core.management import call_command
 from .fixtures import *  # noqa
 
 
+#
+# Load initial taiga fixtures
+#
 @pytest.fixture(scope="session")
 def django_db_setup(django_db_setup, django_db_blocker):
     with django_db_blocker.unblock():
         call_command("loaddata", "initial_project_templates.json", verbosity=0)
+
+
+#
+# Manage slow tests
+#
+def pytest_addoption(parser):
+    parser.addoption("--slow_only", action="store_true", default=False, help="run slow tests only")
+
+    parser.addoption("--fast_only", action="store_true", default=False, help="exclude slow tests")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--slow_only"):
+        skip = pytest.mark.skip(reason="only execute slow test")
+        for item in items:
+            # Only those with django_db(transaction=true)
+            if "django_db" not in item.keywords:
+                item.add_marker(skip)
+            else:
+                for marker in item.iter_markers(name="django_db"):
+                    if not marker.kwargs.get("transaction", False):
+                        item.add_marker(skip)
+                        break
+    elif config.getoption("--fast_only"):
+        skip = pytest.mark.skip(reason="exlcude slow test")
+        for item in items:
+            # Exclude those with django_db(transaction=true)
+            for marker in item.iter_markers(name="django_db"):
+                if marker.kwargs.get("transaction", False):
+                    item.add_marker(skip)
+                    break
--- python/docs/tests.md
+++ python/docs/tests.md
@@ -9,12 +9,29 @@ $ cd python/apps/taiga/
 (taiga)$ pytest
 ```
 
+## Slow vs Fast tests
+
+Tests in which we use the `client` fixture to make API calls must be marked as `django_db(transactional=True)` (more info here [fastapi:issue#2075](https://github.com/tiangolo/fastapi/issues/2075)), for this reason, they are considered slow tests. So we have slow and fast tests and they can run as follow:
+
+- For all the tests:
+  ```shell
+  (taiga)$ pytest
+  ```
+- For slow tests only:
+  ```shell
+  (taiga)$ pytest --slow_only
+  ```
+- For fast test obnly
+  ```shell
+  (taiga)$ pytest --fast_only
+  ```
+
+
 ## Databese
 
 Currently we are reusing the test database (`--reuse-db`) by default to improve the speed.
 
-If you **create some new migration**, remeber to run the test with `--create-db` to drop
-the current db and recreate a new one.
+If you **create some new migration**, remeber to run the test with `--create-db` to drop the current db and recreate a new one.
 
 ```shell
 (taiga)$ pytest --create-db

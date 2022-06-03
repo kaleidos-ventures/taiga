42c8688f3b5d63528a0bc19da3fecc8fe41bccdd
David Barragán Merino <david.barragan@kaleidos.net>
2022-06-03 09:25:48 +0200

    chore(routers): minor refactor

--- python/apps/taiga/src/taiga/routers/routes.py
+++ python/apps/taiga/src/taiga/routers/routes.py
@@ -9,40 +9,48 @@ from fastapi import APIRouter
 from taiga.auth.routing import AuthAPIRouter
 from taiga.exceptions.api.errors import ERROR_401
 
+tags_metadata = []
+
+
 # /auth
 auth = AuthAPIRouter(prefix="/auth", tags=["auth"])
 unauth = APIRouter(prefix="/auth", tags=["auth"], responses=ERROR_401)
-auth_metadata = {
-    "name": "auth",
-    "description": "Enpoints for API authentication.",
-}
+tags_metadata.append(
+    {
+        "name": "auth",
+        "description": "Enpoints for API authentication.",
+    }
+)
 
 # /users
 users = AuthAPIRouter(prefix="/users", tags=["users"])
 unauth_users = APIRouter(prefix="/users", tags=["users"])
-users_metadata = {
-    "name": "users",
-    "description": "Endpoint for users resources.",
-}
+tags_metadata.append(
+    {
+        "name": "users",
+        "description": "Endpoint for users resources.",
+    }
+)
 
 # /projects
 projects = AuthAPIRouter(prefix="/projects", tags=["projects"])
 unauth_projects = APIRouter(prefix="/projects", tags=["projects"])
-projects_metadata = {
-    "name": "projects",
-    "description": "Endpoint for projects resources.",
-}
+tags_metadata.append(
+    {
+        "name": "projects",
+        "description": "Endpoint for projects resources.",
+    }
+)
 
 # /workspaces
 workspaces = AuthAPIRouter(prefix="/workspaces", tags=["workspaces"])
-workspaces_metadata = {
-    "name": "workspaces",
-    "description": "Endpoint for workspaces resources.",
-}
+tags_metadata.append(
+    {
+        "name": "workspaces",
+        "description": "Endpoint for workspaces resources.",
+    }
+)
 
 # /my
 my = AuthAPIRouter(prefix="/my", tags=["my"])
-my_metadata = {"name": "my", "description": "Endpoints for logged-in user's resources."}
-
-
-tags_metadata = [auth_metadata, users_metadata, projects_metadata, workspaces_metadata, my_metadata]
+tags_metadata.append({"name": "my", "description": "Endpoints for logged-in user's resources."})

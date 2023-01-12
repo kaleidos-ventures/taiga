c5cb04b62c56085a37f263ca4151c0f6d133ed2f
Juanfran <juanfran.ag@gmail.com>
2023-01-12 12:52:38 +0100

    fix(translation): generate translation checksum for cache busting

--- javascript/apps/taiga/src/app/transloco/transloco-root.module.ts
+++ javascript/apps/taiga/src/app/transloco/transloco-root.module.ts
@@ -18,13 +18,15 @@ import {
 import { Injectable, NgModule } from '@angular/core';
 import { TranslocoMessageFormatModule } from '@ngneat/transloco-messageformat';
 import { environment } from '~/environments/environment';
+import cacheBusting from '~/assets/i18n/i18n-cache-busting.json';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
   constructor(private http: HttpClient) {}
 
-  public getTranslation(lang: string) {
-    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+  public getTranslation(lang: keyof typeof cacheBusting) {
+    // eslint-disable-next-line
+    return this.http.get<Translation>(`/assets/i18n/${lang}.json?v=${cacheBusting[lang]}`);
   }
 }
 
---
+++ javascript/scripts/i18n-cache-busting.js
@@ -0,0 +1,31 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2021-present Kaleidos Ventures SL
+ */
+
+const crypto = require('crypto');
+const fs = require('fs');
+const glob = require('glob');
+
+function generateChecksum(str, algorithm, encoding) {
+  return crypto
+    .createHash(algorithm || 'md5')
+    .update(str, 'utf8')
+    .digest(encoding || 'hex');
+}
+
+const result = {};
+
+glob.sync(`apps/taiga/src/assets/i18n/**/*.json`).forEach((path) => {
+  const [_, lang] = path.split('src/assets/i18n/');
+  const content = fs.readFileSync(path, { encoding: 'utf-8' });
+  result[lang.replace('.json', '')] = generateChecksum(content);
+});
+
+fs.writeFileSync(
+  'apps/taiga/src/assets/i18n/i18n-cache-busting.json',
+  JSON.stringify(result)
+);

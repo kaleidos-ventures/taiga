8e3ea32f80fd6bba8aed2e8dfa68cfe16a310767
Daniel Herrero García <daniel.herrero@kaleidos.net>
2023-01-26 14:55:31 +0100

    chore(workpaces): delete schemas and use serializers

---
+++ python/apps/taiga/src/taiga/workspaces/workspaces/serializers/services.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2021-present Kaleidos Ventures SL
+
+from taiga.workspaces.workspaces.models import Workspace
+from taiga.workspaces.workspaces.serializers import WorkspaceDetailSerializer, WorkspaceSerializer
+from taiga.workspaces.workspaces.serializers.nested import WorkspaceNestedSerializer
+
+
+def serialize_workspace(workspace: Workspace, user_role: str, total_projects: str) -> WorkspaceSerializer:
+    return WorkspaceSerializer(
+        id=workspace.id,
+        name=workspace.name,
+        slug=workspace.slug,
+        color=workspace.color,
+        total_projects=total_projects,
+        has_projects=workspace.has_projects,  # type: ignore[attr-defined]
+        is_premium=workspace.is_premium,
+        user_role=user_role,
+        user_is_owner=workspace.user_is_owner,  # type: ignore[attr-defined]
+    )
+
+
+def serialize_workspace_detail(workspace: Workspace) -> WorkspaceDetailSerializer:
+    return WorkspaceDetailSerializer(
+        id=workspace.id,
+        name=workspace.name,
+        slug=workspace.slug,
+        color=workspace.color,
+        latest_projects=workspace.latest_projects,  # type: ignore[attr-defined]
+        invited_projects=workspace.invited_projects,  # type: ignore[attr-defined]
+        total_projects=workspace.total_projects,  # type: ignore[attr-defined]
+        has_projects=workspace.has_projects,  # type: ignore[attr-defined]
+        is_premium=workspace.is_premium,
+        user_role=workspace.user_role,  # type: ignore[attr-defined]
+        user_is_owner=workspace.user_is_owner,  # type: ignore[attr-defined]
+    )
+
+
+def serialize_nested(workspace: Workspace, user_role: str) -> WorkspaceNestedSerializer:
+    return WorkspaceNestedSerializer(
+        id=workspace.id,
+        name=workspace.name,
+        slug=workspace.slug,
+        user_role=user_role,
+        is_premium=workspace.is_premium,
+    )
--- python/apps/taiga/src/taiga/workspaces/workspaces/services.py
+++ python/apps/taiga/src/taiga/workspaces/workspaces/services.py
@@ -4,7 +4,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 # Copyright (c) 2021-present Kaleidos Ventures SL
-
 from typing import cast
 from uuid import UUID
 
@@ -16,7 +15,9 @@ from taiga.workspaces.roles import repositories as ws_roles_repositories
 from taiga.workspaces.roles import services as ws_roles_services
 from taiga.workspaces.workspaces import repositories as workspaces_repositories
 from taiga.workspaces.workspaces.models import Workspace
-from taiga.workspaces.workspaces.serializers.related import WorkspaceSummarySerializer
+from taiga.workspaces.workspaces.serializers import WorkspaceDetailSerializer, WorkspaceSerializer
+from taiga.workspaces.workspaces.serializers import services as serializers_services
+from taiga.workspaces.workspaces.serializers.nested import WorkspaceNestedSerializer
 
 ##########################################################
 # create workspace
@@ -41,8 +42,11 @@ async def create_workspace(name: str, color: int, owner: User) -> Workspace:
 ##########################################################
 
 
-async def list_user_workspaces(user: User) -> list[Workspace]:
-    return await workspaces_repositories.list_user_workspaces_overview(user=user)
+async def list_user_workspaces(user: User) -> list[WorkspaceDetailSerializer]:
+    return [
+        serializers_services.serialize_workspace_detail(workspace=workspace)
+        for workspace in await workspaces_repositories.list_user_workspaces_overview(user=user)
+    ]
 
 
 ##########################################################
@@ -54,21 +58,21 @@ async def get_workspace(id: UUID) -> Workspace | None:
     return await workspaces_repositories.get_workspace(filters={"id": id})
 
 
-async def get_workspace_detail(id: UUID, user_id: UUID | None) -> Workspace | None:
-    user_workspace_role_name = await ws_roles_services.get_workspace_role_name(workspace_id=id, user_id=user_id)
-    user_projects_count = await projects_repositories.get_total_projects(
-        filters={"workspace_id": id, "project_or_workspace_member_id": user_id},
-    )
-    return await workspaces_repositories.get_workspace_detail(
-        filters={"id": id},
-        user_id=user_id,
-        user_workspace_role_name=user_workspace_role_name,
-        user_projects_count=user_projects_count,
-    )
+async def get_workspace_detail(id: UUID, user_id: UUID | None) -> WorkspaceSerializer | None:
+    workspace = await workspaces_repositories.get_workspace_detail(filters={"id": id}, user_id=user_id)
+    if workspace:
+        return serializers_services.serialize_workspace(
+            workspace=workspace,
+            user_role=await ws_roles_services.get_workspace_role_name(workspace_id=id, user_id=user_id),
+            total_projects=await projects_repositories.get_total_projects(
+                filters={"workspace_id": id, "project_or_workspace_member_id": user_id}
+            ),
+        )
+
+    return None
 
 
-# TODO: change this name to `get_workspace_nested`
-async def get_workspace_summary(id: UUID, user_id: UUID | None) -> WorkspaceSummarySerializer:
+async def get_workspace_nested(id: UUID, user_id: UUID | None) -> WorkspaceNestedSerializer:
     # TODO: this service should be improved
     user_workspace_role_name = await ws_roles_services.get_workspace_role_name(workspace_id=id, user_id=user_id)
     workspace = cast(
@@ -77,15 +81,12 @@ async def get_workspace_summary(id: UUID, user_id: UUID | None) -> WorkspaceSumm
             filters={"id": id},
         ),
     )
-    # TODO: take this code to a workspaces/serializers/services.py
-    return WorkspaceSummarySerializer(
-        id=workspace.id,
-        name=workspace.name,
-        slug=workspace.slug,
-        user_role=user_workspace_role_name,
-        is_premium=workspace.is_premium,
-    )
+
+    return serializers_services.serialize_nested(workspace=workspace, user_role=user_workspace_role_name)
 
 
-async def get_user_workspace(user: User, id: UUID) -> Workspace | None:
-    return await workspaces_repositories.get_user_workspace_overview(user=user, id=id)
+async def get_user_workspace(user: User, id: UUID) -> WorkspaceDetailSerializer | None:
+    workspace = await workspaces_repositories.get_user_workspace_overview(user=user, id=id)
+    if workspace:
+        return serializers_services.serialize_workspace_detail(workspace=workspace)
+    return None

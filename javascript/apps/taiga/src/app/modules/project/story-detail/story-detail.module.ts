f19199b0026603a39494f3ed93beeb18c2809cfa
Xaviju <xaviju@gmail.com>
2023-04-11 13:25:10 +0200

    fix(story): navigation arrows order in rtl

--- javascript/apps/taiga/src/app/modules/project/story-detail/story-detail.module.ts
+++ javascript/apps/taiga/src/app/modules/project/story-detail/story-detail.module.ts
@@ -20,30 +20,30 @@ import {
 } from '@taiga-ui/core';
 import { InputsModule } from '@taiga/ui/inputs/inputs.module';
 import { ModalModule } from '@taiga/ui/modal';
+import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
 import { CommonTemplateModule } from '~/app/shared/common-template.module';
 import { AutoFocusDirective } from '~/app/shared/directives/auto-focus/auto-focus.directive';
+import { ClickActionAreaDirective } from '~/app/shared/directives/click-action-area/click-action-area.directive';
+import { CodeHightlightDirective } from '~/app/shared/directives/code-highlight/code-highlight.directive';
 import { HasPermissionDirective } from '~/app/shared/directives/has-permissions/has-permission.directive';
+import { RestoreFocusTargetDirective } from '~/app/shared/directives/restore-focus/restore-focus-target.directive';
+import { RestoreFocusDirective } from '~/app/shared/directives/restore-focus/restore-focus.directive';
+import { DiscardChangesModalComponent } from '~/app/shared/discard-changes-modal/discard-changes-modal.component';
 import { DropdownModule } from '~/app/shared/dropdown/dropdown.module';
 import { DateDistancePipe } from '~/app/shared/pipes/date-distance/date-distance.pipe';
+import { SafeHtmlPipe } from '~/app/shared/pipes/safe-html/safe-html.pipe';
 import { StatusColorPipe } from '~/app/shared/pipes/status-color/status-color.pipe';
 import { ResizedDirective } from '~/app/shared/resize/resize.directive';
 import { UserAvatarComponent } from '~/app/shared/user-avatar/user-avatar.component';
 import { AssignUserComponent } from '../components/assign-user/assign-user.component';
+import { FieldConflictComponent } from '../components/field-conflict/field-conflict.component';
 import { StoryDetailAssignComponent } from './components/story-detail-assign/story-detail-assign.component';
+import { StoryDetailScriptionStickyDirective } from './components/story-detail-description/story-detail-description-sticky.directive';
+import { StoryDetailDescriptionComponent } from './components/story-detail-description/story-detail-description.component';
 import { StoryDetailStatusComponent } from './components/story-detail-status/story-detail-status.component';
+import { StoryDetailTitleComponent } from './components/story-detail-title/story-detail-title.component';
 import { DataAccessStoryDetailModule } from './data-access/story-detail-data-access.module';
 import { StoryDetailComponent } from './story-detail.component';
-import { StoryDetailTitleComponent } from './components/story-detail-title/story-detail-title.component';
-import { RestoreFocusDirective } from '~/app/shared/directives/restore-focus/restore-focus.directive';
-import { DiscardChangesModalComponent } from '~/app/shared/discard-changes-modal/discard-changes-modal.component';
-import { RestoreFocusTargetDirective } from '~/app/shared/directives/restore-focus/restore-focus-target.directive';
-import { FieldConflictComponent } from '../components/field-conflict/field-conflict.component';
-import { StoryDetailDescriptionComponent } from './components/story-detail-description/story-detail-description.component';
-import { EditorModule, TINYMCE_SCRIPT_SRC } from '@tinymce/tinymce-angular';
-import { ClickActionAreaDirective } from '~/app/shared/directives/click-action-area/click-action-area.directive';
-import { SafeHtmlPipe } from '~/app/shared/pipes/safe-html/safe-html.pipe';
-import { StoryDetailScriptionStickyDirective } from './components/story-detail-description/story-detail-description-sticky.directive';
-import { CodeHightlightDirective } from '~/app/shared/directives/code-highlight/code-highlight.directive';
 
 @NgModule({
   imports: [

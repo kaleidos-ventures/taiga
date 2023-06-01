a03deee99fe0438074dd36d005d86d9a7ff5c85a
Marina López <marina.lopez.yap@gmail.com>
2023-06-01 10:51:34 +0200

    feat(front): u#351 t#3697 avatar tests

---
+++ javascript/libs/ui/src/lib/avatar/avatar.component.spec.ts
@@ -0,0 +1,44 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2023-present Kaleidos INC
+ */
+
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { AvatarComponent } from './avatar.component';
+import { RandomColorService } from '../services/random-color/random-color.service';
+
+describe('AvatarComponent', () => {
+  let spectator: Spectator<AvatarComponent>;
+  const createComponent = createComponentFactory({
+    component: AvatarComponent,
+    mocks: [RandomColorService],
+    providers: [],
+  });
+
+  beforeEach(() => {
+    spectator = createComponent({
+      detectChanges: false,
+      props: {},
+      providers: [],
+    });
+  });
+
+  it('setAvatarName: input Norma Fisher -> ouput N M', () => {
+    expect(spectator.component.setAvatarName('Norma Fisher')).toEqual('N F');
+  });
+
+  it('setAvatarName: input / Norma Fisher -> ouput / N', () => {
+    expect(spectator.component.setAvatarName('/ Norma Fisher')).toEqual('/ N');
+  });
+
+  it('setAvatarName: input Norma ? Fisher -> ouput N ?', () => {
+    expect(spectator.component.setAvatarName('Norma ? Fisher')).toEqual('N ?');
+  });
+
+  it('setAvatarName: input Norma 🤖 Fisher -> ouput N', () => {
+    expect(spectator.component.setAvatarName('Norma 🤖 Fisher')).toEqual('N');
+  });
+});

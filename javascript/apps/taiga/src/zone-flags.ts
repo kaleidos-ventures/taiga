0af71c81341a7298e35f324ad119bd9d0fb71789
Juanfran <juanfran.ag@gmail.com>
2023-08-02 09:58:24 +0200

    fix: i#3862 enable scroll zone

--- javascript/apps/taiga/src/zone-flags.ts
+++ javascript/apps/taiga/src/zone-flags.ts
@@ -11,4 +11,3 @@ import { zoneConfig } from '@rx-angular/cdk/zone-configurations';
 zoneConfig.global.disable.XHR();
 zoneConfig.global.disable.blocking();
 zoneConfig.global.disable.on_property();
-zoneConfig.events.disable.UNPATCHED_EVENTS(['scroll', 'wheel']);

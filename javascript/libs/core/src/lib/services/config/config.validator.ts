863c3ab677c1807df5dd3b179d2ee63a9eb76f93
Marina López <marina.lopez.yap@gmail.com>
2023-08-09 14:41:46 +0200

    fix(config): i#3908 set default values config

--- javascript/libs/core/src/lib/services/config/config.validator.ts
+++ javascript/libs/core/src/lib/services/config/config.validator.ts
@@ -9,24 +9,36 @@
 import * as z from 'zod';
 
 export const ConfigValidator = z.object({
-  api: z.string(),
-  ws: z.string(),
-  supportEmail: z.string(),
-  emailWs: z.string().optional(),
-  social: z.object({
-    github: z.object({
-      serverUrl: z.string().optional(),
-      clientId: z.string(),
+  api: z.string().url().default('http://127.0.0.1:8000/api/v2'),
+  ws: z.string().url().default('ws://127.0.0.1:8000/events/'),
+  supportEmail: z.string().email().default('support@taiga.io'),
+  emailWs: z.string().email().optional(),
+  social: z
+    .object({
+      github: z.object({
+        serverUrl: z.string().url().optional(),
+        clientId: z.string().default(''),
+      }),
+      gitlab: z.object({
+        serverUrl: z.string().url().optional(),
+        clientId: z.string().default(''),
+      }),
+      google: z.object({
+        serverUrl: z.string().url().optional(),
+        clientId: z.string().default(''),
+      }),
+    })
+    .default({
+      github: {
+        clientId: '',
+      },
+      gitlab: {
+        clientId: '',
+      },
+      google: {
+        clientId: '',
+      },
     }),
-    gitlab: z.object({
-      serverUrl: z.string().optional(),
-      clientId: z.string(),
-    }),
-    google: z.object({
-      serverUrl: z.string().optional(),
-      clientId: z.string(),
-    }),
-  }),
-  accessTokenLifetime: z.number(),
-  globalBanner: z.boolean(),
+  accessTokenLifetime: z.number().default(180),
+  globalBanner: z.boolean().default(false),
 });

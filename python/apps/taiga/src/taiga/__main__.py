c09c2420beb5e47f3329e151151a9f0fb141a000
Yamila Moreno <yamila.ms@gmail.com>
2023-08-21 14:14:00 +0200

    chore: fix minor typos

--- python/apps/taiga/src/taiga/__main__.py
+++ python/apps/taiga/src/taiga/__main__.py
@@ -6,12 +6,6 @@
 #
 # Copyright (c) 2023-present Kaleidos INC
 
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-# Copyright (c) 2023-present Kaleidos INC
-
 # flake8: noqa: E402
 
 from taiga.base.django import setup_django
